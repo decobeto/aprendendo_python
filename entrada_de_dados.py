48d59f81b04ef395e4a7f7038c01af1b0bf948cc
André Roberto <32197501+decobeto@users.noreply.github.com>
2019-06-06 16:16:48 -0300

    entrada de dados

---
+++ entrada_de_dados.py
@@ -0,0 +1,7 @@
+# Conversão de temperatura
+
+temperaturaFahrenheit = input("Digite uma temperatura em Fahrenheit: ")
+
+temperaturaCelsius = (float(temperaturaFahrenheit) - 32) * 5 / 9
+
+print("A temperatura em celsius é ", temperaturaCelsius)
