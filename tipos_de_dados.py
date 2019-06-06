df57f174cd84e3b9183c5c0ec959be4bb5d2aa86
André Roberto <32197501+decobeto@users.noreply.github.com>
2019-06-06 11:46:23 -0300

    Create tipos_de_dados.py

---
+++ tipos_de_dados.py
@@ -0,0 +1,22 @@
+type(10) #int
+type('10') #string
+type(5/2) #float
+
+10 // 3 # retorna num inteiro
+10 % 3 # resto
+
+peso = 78
+altura = 1.83
+
+type(altura)
+
+imc = peso / (altura ** 2)
+
+IMCInteiro = int(imc) # converter para valor inteiro
+
+texto = "Bom dia, tudo bem?"
+
+len(texto) # len = length - tamanho do texto
+
+temp = str(imc) #converter para string
+len(temp)
