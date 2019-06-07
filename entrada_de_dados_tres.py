1230c749cabd51d26908c217887f41c02cc9348a
André Roberto <32197501+decobeto@users.noreply.github.com>
2019-06-07 00:54:27 -0300

    entrada de dados

--- entrada_de_dados_tres.py
+++ entrada_de_dados_tres.py
@@ -2,9 +2,11 @@
 segundos_str = input("Por favor, entre com o número de segundos que deseja converter: ")
 total_segs = int(segundos_str)
 
-horas = total_segs // 3600
+dias = total_segs // 86400
+horas_restantes = total_segs % 86400
+horas = horas_restantes // 3600
 segs_restantes = total_segs % 3600
 minutos = segs_restantes // 60
 segs_restantes_final = segs_restantes % 60
 
-print(horas, "horas, ", minutos, "minutos e", segs_restantes_final, "segundos.")
+print(dias, "dias, ", horas, "horas, ", minutos, "minutos e", segs_restantes_final, "segundos.")
