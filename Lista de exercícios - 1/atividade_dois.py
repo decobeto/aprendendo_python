34de989f99a2a17d5e3f0a56702407402db6134f
André Roberto <32197501+decobeto@users.noreply.github.com>
2019-06-27 14:50:36 -0300

    lista

---
+++ Lista de exercícios - 1/atividade_dois.py
@@ -0,0 +1,15 @@
+primeiraNota = input("Digite a primeira nota: ")
+primeiraNota = int(primeiraNota)
+
+segundaNota = input("Digite a segunda nota: ")
+segundaNota = int(segundaNota)
+
+terceiraNota = input("Digite a terceira nota: ")
+terceiraNota = int(terceiraNota)
+
+quartaNota = input("Digite a quarta nota: ")
+quartaNota = int(quartaNota)
+
+mediaAritmetica = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4
+
+print("A média aritmética é", mediaAritmetica)
---
+++ Lista de exercícios - 1/atividade_um.py
@@ -0,0 +1,8 @@
+valorLadoQuadrado = input("Digite o valor correspondente ao lado  de um quadrado: ")
+
+valorLadoQuadrado = int(valorLadoQuadrado)
+
+area = valorLadoQuadrado * valorLadoQuadrado
+perimetro = valorLadoQuadrado + valorLadoQuadrado + valorLadoQuadrado + valorLadoQuadrado
+
+print("perímetro:", perimetro, "-", "área:", area)
