9a7504592de63380d676bb2e9854655d59cb27ea
André Roberto <andreroberto52@gmail.com>
2019-08-02 11:13:07 -0300

    expressões condicionais em python

---
+++ execucao_condicional.py
@@ -0,0 +1,8 @@
+temperatura = 102
+
+if temperatura > 100:
+  aguaFerve = True
+  evaporacao = "Muito Rápida"
+
+print(aguaFerve)
+print(evaporacao)
---
+++ expressao_condicional_dois.py
@@ -0,0 +1,7 @@
+tempoDeJogo = int(input("Quanto tempo temos já jogado? "))
+
+if tempoDeJogo <= 90:
+  print("Ainda tem jogo pela frente")
+  print("Que bom eu adoro algum esporte")
+else:
+  print("Acabou o jogo")
